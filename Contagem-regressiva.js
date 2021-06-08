77a1e038d8f262ed8f107577906b0f8fb16cdac4
Bruno de Lucas Barbosa <brunnobarbosas@gmail.com>
2021-06-08 17:05:36 -0300

    Adicionei a quarta atividade

---
+++ Contagem-regressiva.js
@@ -0,0 +1,12 @@
+function solucao(numero) {
+    for(let i = numero; i > -1; i--) {
+      console.log(i)
+      if (i == 0) {
+        console.log("KABUM")
+      }
+    }
+    if(numero == 0){
+      console.log("KABUM")
+    }
+        
+    }
